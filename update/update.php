b9cfe747577430b776d361c65bf985072abfc04a
Réda YAHYAOUI <yahyaouir@yahoo.frgit config --global user.name Reda21-eng>
2025-01-30 17:30:27 +0100

    update

--- update/update.php
+++ update/update.php
@@ -1,5 +1,11 @@
 <?php
 
+$connexion = new PDO( 'mysql:host=localhost;dbname=gestionnaire-de-menus', 'root', '' );
+if(!$connexion){
+        die("Erreur : La connexion a échoué.");
+}
+ session_start();
+
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -10,6 +16,26 @@
     <link rel="stylesheet" href="css/style.css">
     <title>Modifier</title>
 </head>
+<header>
+    <nav>
+        <a href = "index.php">Accueil</a>
+        <a href = "login.php"> Connexion</a>
+        <a href = "delete.php"> Supprimer</a>
+        <a href = "update.php"> Modifier</a>
+    </nav>
+ </header> 
+ <body>
+     <form action="create.php" method="post">
+         <label for="create.php">Modifier votre menu</label>
+         <input type="text" id="menu" name="menu" required>
+        </form>
+    
+    <img src="Spaghettis" alt = "Plat de spaghettis">
+    <img src="Plat" alt = "Plat ">
+    <img src="Riz" alt = "Riz">
+    
+ </body>
+ </html>
 <body>
     <p>Modifier, <?php echo $_SESSION[""]?></p>
 

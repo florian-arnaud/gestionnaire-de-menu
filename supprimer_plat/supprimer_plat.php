20b155d60c07c27bc7b372c56b77ef0d5e8e9ceb
Réda YAHYAOUI <yahyaouir@yahoo.frgit config --global user.name Reda21-eng>
2025-02-03 12:02:13 +0100

    supprimer_plat

---
+++ supprimer_plat/supprimer_plat.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+require_once "config/connexionBDD.php";
+
+if($_SESSION['droits'] != 2)
+{
+    header('Location: index.php');
+    exit();
+}
+
+$id_plat = trim(htmlspecialchars($_GET['id']));
+$requete = $connexion->prepare("DELETE FROM plats WHERE id_plat = :id_plat");
+$requete->execute(['id_plat' => $id_plat]); 
+if ($requete) {
+    header('Location: plats.php');
+} else {
+    echo "La suppression n'a pas fonctionné.";
+}
+?>

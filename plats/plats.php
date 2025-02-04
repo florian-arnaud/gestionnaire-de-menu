a2f69830395329643e4ef7777c58b50e0010074e
Réda YAHYAOUI <yahyaouir@yahoo.frgit config --global user.name Reda21-eng>
2025-02-04 14:03:52 +0100

    liens fichiers et CSS modifiés

--- plats/plats.php
+++ plats/plats.php
@@ -33,7 +33,7 @@ $resultat = $requete->fetchAll(PDO::FETCH_ASSOC);
                 </div>
                 <?php if (!isset($_SESSION['nom_utilisateur'])) {
                     echo "<li><a href='http://localhost/gestionnaire-de-menu/login/login.php'>Connexion</a></li>";
-                    echo "<li><a href='http://localhost/gestionnaire-de-menu/inscription/inscription.php'>S'inscrire</a></li>";
+                    echo "<li><a href='http://localhost/gestionnaire-de-menu/inscription/inscription.php'gi>S'inscrire</a></li>";
                 } else {
                     echo "<div>";
                     echo "<li><a href='#'>" . $_SESSION['nom_utilisateur'] . "</a></li>";
